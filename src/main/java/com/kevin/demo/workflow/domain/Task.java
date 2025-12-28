eed4a84c4400bc9564e75daf465998ed66ef8a77
KevinHeinz <36209706+KevinHeinz@users.noreply.github.com>
2025-12-28 17:11:43 -0600

    combine Entity persistence imports

--- src/main/java/com/kevin/demo/workflow/domain/Task.java
+++ src/main/java/com/kevin/demo/workflow/domain/Task.java
@@ -2,16 +2,7 @@ package com.kevin.demo.workflow.domain;
 
 import java.time.Instant;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tasks")
