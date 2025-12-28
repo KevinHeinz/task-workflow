796eadfa730fccade20424fa9173c6873e85f5fb
KevinHeinz <36209706+KevinHeinz@users.noreply.github.com>
2025-12-28 15:57:03 -0600

    Initial task-workflow scaffold

---
+++ src/main/java/com/kevin/demo/workflow/TaskWorkflowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.demo.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskWorkflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskWorkflowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/demo/workflow/TaskWorkflowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.demo.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskWorkflowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
